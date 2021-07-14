f1411384deb7a43ba56a9e0ea66e4c191a50be36
Dayle Salmon <daylesalmon@gmail.com>
2021-07-14 15:36:06 +0100

    Updated build command

--- config-overrides.js
+++ config-overrides.js
@@ -1,29 +1,30 @@
-// const { override, addWebpackAlias, disableChunk } = require('customize-cra');
-
-// module.exports = override(
-//   addWebpackAlias({
-//     react: 'preact/compat',
-//     'react-dom/test-utils': 'preact/test-utils',
-//     'react-dom': 'preact/compat',
-//   }),
-
-//   disableChunk()
-// );
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   // The Webpack config to use when compiling your react app for development or production.
   webpack: function (config, env) {
-    // const config = {
-    //   //...snip
-    //   resolve: {
-    //     alias: {
-    //       react: 'preact/compat',
-    //       'react-dom/test-utils': 'preact/test-utils',
-    //       'react-dom': 'preact/compat',
-    //       // Must be below test-utils
-    //     },
-    //   },
-    // };
+    config.optimization.runtimeChunk = false;
+    config.optimization.splitChunks = {
+      cacheGroups: {
+        default: false,
+      },
+    };
+
+    // JS
+    config.output.filename = 'static/js/build.js';
+    // CSS remove MiniCssPlugin
+    // config.plugins = config.plugins.filter(plugin => !(plugin instanceof MiniCssExtractPlugin));
+    // // CSS replaces all MiniCssExtractPlugin.loader with style-loader
+    // config.module.rules[2].oneOf = config.module.rules[2].oneOf.map(rule => {
+    //   if (!rule.hasOwnProperty('use')) return rule;
+    //   return Object.assign({}, rule, {
+    //     use: rule.use.map(options =>
+    //       /mini-css-extract-plugin/.test(options.loader)
+    //         ? { loader: require.resolve('style-loader'), options: {} }
+    //         : options
+    //     ),
+    //   });
+    // });
 
     config.resolve.alias = {
       ...config.resolve.alias,
