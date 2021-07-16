fcd18a83d9ae46b717416922c4b49e46d3914b2b
Dayle Salmon <daylesalmon@gmail.com>
2021-07-16 11:58:46 +0100

    Tidied up personalisedView

--- src/components/ViewsToShow/PersonalisedView/PersonalisedView.tsx
+++ src/components/ViewsToShow/PersonalisedView/PersonalisedView.tsx
@@ -7,14 +7,14 @@ import TramRow from './TramRow/TramRow';
 import TrainRow from './TrainRow/TrainRow';
 
 const PersonalisedView = (): JSX.Element => {
-  const currentFavs = getFavouritesFromCookies();
+  const { bus, tram, train } = getFavouritesFromCookies();
 
   // Check if we have any favs for each mode, if so then show the relevant row
   return (
     <>
-      {currentFavs.bus && currentFavs.bus?.length > 0 && <BusRow favs={currentFavs.bus} />}
-      {currentFavs.tram && currentFavs.tram?.length > 0 && <TramRow favs={currentFavs.tram} />}
-      {currentFavs.train && currentFavs.train?.length > 0 && <TrainRow favs={currentFavs.train} />}
+      {bus && bus?.length > 0 && <BusRow favs={bus} />}
+      {tram && tram?.length > 0 && <TramRow favs={tram} />}
+      {train && train?.length > 0 && <TrainRow favs={train} />}
 
       <PersonalisedCTAButtons />
     </>
