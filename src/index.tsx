73981a1db1a228f73916b7f1cf7c154fd261f971
Dayle Salmon <daylesalmon@gmail.com>
2021-07-19 15:16:09 +0100

    Updated the way the app builds for versioning

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import 'preact/debug';
 import habitat from 'preact-habitat';
 import App from './components/App/App';
 
+// eslint-disable-next-line no-console
+console.log(`Loaded ${process.env.REACT_APP_NAME} - v${process.env.REACT_APP_VERSION}`);
+
 habitat(App).render({
   selector: '[data-widget-host="habitat"]',
   clean: true,
