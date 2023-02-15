02754328f1ac75132aa76e3a18c98295611c205e
Sudheer <92720228+sudheer-rk@users.noreply.github.com>
2023-02-15 08:43:19 +0000

    fix: ds/fix/update disruptions api params (#22)
    * chore(release): 1.0.0 [skip ci]
    
    # 1.0.0 (2021-07-20)
    
    ### Bug Fixes
    
    * Updated env vars ([#1](https://github.com/wmcadigital/tfwm-travel-updates-widget/issues/1)) ([bbcc7a0](https://github.com/wmcadigital/tfwm-travel-updates-widget/commit/bbcc7a0d8951b0740b8ee67cd98eac060970e7b2))
    * Updated semantic release configuration ([#3](https://github.com/wmcadigital/tfwm-travel-updates-widget/issues/3)) ([bdf1d9e](https://github.com/wmcadigital/tfwm-travel-updates-widget/commit/bdf1d9e60f2bab27f8673096547483fb22a0a784))
    
    * chore(release): 1.1.0 [skip ci]
    
    # [1.1.0](https://github.com/wmcadigital/tfwm-travel-updates-widget/compare/v1.0.0...v1.1.0) (2021-07-22)
    
    ### Bug Fixes
    
    * Updated data-name of where preact gets injected ([#11](https://github.com/wmcadigital/tfwm-travel-updates-widget/issues/11)) ([dad8890](https://github.com/wmcadigital/tfwm-travel-updates-widget/commit/dad8890fb494164e68493487a25a003af6021ef5))
    
    ### Features
    
    * Added accordion for mobile view ([#9](https://github.com/wmcadigital/tfwm-travel-updates-widget/issues/9)) ([720ae55](https://github.com/wmcadigital/tfwm-travel-updates-widget/commit/720ae55bcd5851a5199b0b26a7a82bfb6fe86bf1))
    
    * chore(release): 1.2.0 [skip ci]
    
    # [1.2.0](https://github.com/wmcadigital/tfwm-travel-updates-widget/compare/v1.1.0...v1.2.0) (2021-07-29)
    
    ### Features
    
    * Personal view now shows any location and radiuses saved via favourites for roads ([#12](https://github.com/wmcadigital/tfwm-travel-updates-widget/issues/12)) ([f030c78](https://github.com/wmcadigital/tfwm-travel-updates-widget/commit/f030c782cef87b0135ff302a482831c5c09d4cf7))
    
    * pass from and to date params to  wmca api portal
    
    * Restyled by prettier-markdown (#23)
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    ---------
    
    Co-authored-by: semantic-release-bot <semantic-release-bot@martynus.net>
    Co-authored-by: Dayle Salmon <13015201+daylesalmon@users.noreply.github.com>
    Co-authored-by: Sudheer Kotha <sudheer.kotha@wmca@org.uk>
    Co-authored-by: restyled-io[bot] <32688539+restyled-io[bot]@users.noreply.github.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

--- src/components/ViewsToShow/DefaultView/DefaultView.tsx
+++ src/components/ViewsToShow/DefaultView/DefaultView.tsx
@@ -6,15 +6,33 @@ import ModalRow from './ModalRow/ModalRow';
 // Types
 import ResponseOrNull from './types';
 
+const currentDate = new Date();
+currentDate.setHours(0, 0, 0, 0);
+
+const from = currentDate.toISOString();
+
+const endOfDay = new Date(
+  currentDate.getFullYear(),
+  currentDate.getMonth(),
+  currentDate.getDate(),
+  23,
+  59,
+  59
+);
+endOfDay.setHours(endOfDay.getHours() + 2);
+const to = endOfDay.toISOString();
+
 const DefaultView = (): JSX.Element => {
-  const { response, isFetching } = useFetch<ResponseOrNull>(`/Disruption/v2/count`);
+  const { response, isFetching } = useFetch<ResponseOrNull>(
+    `/Disruption/v2/count?from=${from}&to=${to}`
+  );
 
   return (
     <>
-      <ModalRow isFetching={isFetching} response={response} mode="bus" />
-      <ModalRow isFetching={isFetching} response={response} mode="train" />
-      <ModalRow isFetching={isFetching} response={response} mode="tram" />
-      <ModalRow isFetching={isFetching} response={response} mode="roads" />
+      <ModalRow isFetching={isFetching} response={response} mode="bus" from={from} to={to} />
+      <ModalRow isFetching={isFetching} response={response} mode="train" from={from} to={to} />
+      <ModalRow isFetching={isFetching} response={response} mode="tram" from={from} to={to} />
+      <ModalRow isFetching={isFetching} response={response} mode="roads" from={from} to={to} />
       <CTAButtons />
     </>
   );
